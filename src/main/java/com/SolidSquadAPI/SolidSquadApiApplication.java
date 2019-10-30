882ac65b483f2de7daa0e52b3e5d6f4b3466c0c4
CreatorBailey <baileym1215@gmail.com>
2019-10-30 13:10:17 -0400

    Initial commit

---
+++ src/main/java/com/SolidSquadAPI/SolidSquadApiApplication.java
@@ -0,0 +1,13 @@
+package com.SolidSquadAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidSquadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidSquadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SolidSquadAPI/SolidSquadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SolidSquadAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidSquadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
